e6187a1bfb5d8ae1469946d20469f88833f3ba96
PavlenkoDm <dmpavlenko07@gmail.com>
2023-05-01 18:23:05 +0300

    update

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,3 @@ export * from './imagegallery/ImageGallery';
 export * from './imagegalleryitem/ImageGalleryItem';
 export * from './button/Button';
 export * from './modal/Modal';
-export * from './loader/Loader';
